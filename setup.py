1a8d707523a4b5b46ffbc3380c09e43a15aed533
Marceau <git@marceau-h.fr>
2024-01-19 16:35:25 +0100

    setup update for new release

--- setup.py
+++ setup.py
@@ -8,27 +8,28 @@ def parse_requirements(req_file):
 
 
 NAME = "pellipop"
-VERSION = "0.3.1"
+VERSION = "0.4.0"
 
 # Get dependencies from requirement files
 SETUP_REQUIRES = ['setuptools', 'setuptools-git', 'wheel']
 INSTALL_REQUIRES = parse_requirements('requirements.txt')
 LONG_DESCRIPTION = ""
 
-with open(os.path.join(os.path.dirname(__file__), 'description.md'), 'r') as f:
+with open(os.path.join(os.path.dirname(__file__), 'readme.md'), 'r') as f:
     LONG_DESCRIPTION = f.read()
 
 def setup_package():
     metadata = dict(name=NAME,
                     version=VERSION,
-                    licence='MPL-2.0 license',
+                    license='MPL-2.0 license',
                     install_requires=INSTALL_REQUIRES,
                     long_description=LONG_DESCRIPTION,
                     long_description_content_type='text/markdown',
                     setup_requires=SETUP_REQUIRES,
                     entry_points={
-                        'console_scripts':[
-                            'pellipop = pellipop.main:start'
+                        'console_scripts': [
+                            'pellipop = pellipop.gui:main',
+                            'pellipop-cli = pellipop.cli:main'
                         ]
                     },
                     packages=find_packages())
