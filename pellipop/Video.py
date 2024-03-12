ec434377ff653168335309e30bbfbdebc1f60fd1
Marceau-h <git@marceau-h.fr>
2024-03-12 11:43:22 +0100

    Abstract video class and DummyVideo to bypass some assertions and display an example of the stem renaming functionalities

--- pellipop/Video.py
+++ pellipop/Video.py
@@ -1,10 +1,13 @@
 import json
 import subprocess
+from abc import ABC
 from pathlib import Path as Path_
 from typing import List, Optional
 
 from pellipop.path_fixer import Path
-class Video:
+
+
+class ABC_Video(ABC):
     probe = "ffprobe -v panic -show_streams -of json"  # Using ffmpeg to get video infos
 
     def __init__(
@@ -14,24 +17,14 @@ class Video:
             offset: int = 0,
             parents_in_name: int = 0
     ):
-        assert isinstance(path, Path_), f"{path} is not a Path object"
-        assert path.is_file(), f"{path} is not a file"
-        assert path.exists(), f"{path} does not exist"
-
-        assert isinstance(reduce, int), f"{reduce} is not an int"
-        assert reduce >= -1, f"{reduce} is not a valid reduce value"
-
-        assert isinstance(offset, int), f"{offset} is not an int"
-        assert offset >= 0, f"{offset} is not a valid offset value"
-
         self.path: Path = path
 
         self.reduce: int = reduce
         self.offset: int = offset
-        self.final_stem: Optional[str] = None
-        self.get_reduce()
         self.parents_in_name: int = parents_in_name
-        self.get_parents()
+
+        self.reduced_stem_attr: str = self.path.stem
+        self.final_stem_attr: Optional[str] = None
 
         self.images: List[Path] = []
         self.image_folder: Optional[Path] = None
@@ -46,8 +39,6 @@ class Video:
         self.fps: Optional[int] = None
         self.channels: Optional[int] = None
         self.rate: Optional[int] = None
-
-        self.get_info()
 
     @property
     def name(self) -> str:
@@ -72,19 +63,25 @@ class Video:
     def with_suffix(self, *args, **kwargs) -> Path:
         return self.path.with_suffix(*args, **kwargs)
 
-    def get_reduce(self) -> None:
+    def get_reduce(self) -> str:
         if self.reduce != -1 or self.offset:
             offset = min(self.offset, len(self.stem) - 1)
-            reduce = min(self.reduce + offset, len(self.stem) - 1) if self.reduce != -1 else -1
-            self.final_stem = self.stem[offset:reduce]
+            reduce = min(self.reduce + offset, len(self.stem) - 1) if self.reduce != -1 else None
+            self.reduced_stem_attr = self.stem[offset:reduce]
+            return self.reduced_stem_attr
+        return self.stem
 
-    def get_parents(self) -> None:
+    def get_parents(self) -> str:
         if not self.parents_in_name:
-            return
+            return self.get_reduce()
 
-        parents = [e.name for e in self.path.parents if e.name not in [".", "..", "", "C:", "D:", "E:"] and e]
+        parents = [e.name for e in self.path.parents if e.name not in [".", "..", "", "C:", "D:", "E:"] and e is not None]
         parents = parents[:min(self.parents_in_name, len(parents))][::-1]
-        self.final_stem = "_".join([p for p in parents] + [self.final_stem])
+        if not parents:
+            return self.get_reduce()
+        self.final_stem_attr = "_".join([p for p in parents] + [self.get_reduce()])
+
+        return self.final_stem_attr
 
     def get_info(self) -> None:
         command = f'{self.probe} "{self.path}"'
@@ -108,20 +105,20 @@ class Video:
             old_folder: Optional[Path] = None,
             new_folder: Optional[Path] = None
     ) -> Path:
-        if not self.final_stem:
+        if not self.final_stem_attr:
             return path
-        path = path.rename(path.with_stem(path.stem.replace(self.stem, self.final_stem)))
+        path = path.rename(path.with_stem(path.stem.replace(self.stem, self.final_stem_attr)))
         if old_folder and new_folder:
             path = path.rename(new_folder / path.name)
         return path
 
     def rename_to_final_images(self) -> None:
-        if not self.final_stem:
+        if not self.final_stem_attr:
             return
 
         old_folder = self.image_folder
 
-        self.image_folder = self.image_folder.parent / self.final_stem.replace(" ", "_")
+        self.image_folder = self.image_folder.parent / self.final_stem_attr.replace(" ", "_")
         self.image_folder.mkdir(parents=True, exist_ok=True)
         self.images = [
             self.rename_to_final(img, old_folder=old_folder, new_folder=self.image_folder)
@@ -145,6 +142,123 @@ class Video:
 
         if self.csv:
             self.csv = self.rename_to_final(self.csv)
+
+    @property
+    def reduced_stem(self) -> str:
+        return self.get_reduce()
+
+    @property
+    def final_stem(self) -> str:
+        return self.get_parents()
+
+    @property
+    def posix(self) -> str:
+        return self.path.as_posix()
+
+    # @property
+    # def reduce(self) -> int:
+    #     return self.reduce_attr
+    #
+    # @reduce.setter
+    # def reduce(self, value: int) -> None:
+    #     assert isinstance(value, int), f"{value} is not an int"
+    #     assert value >= -1, f"{value} is not a valid reduce value"
+    #     self.reduce_attr = value
+    #     self.get_reduce()
+    #
+    # @property
+    # def offset(self) -> int:
+    #     return self.offset_attr
+    #
+    # @offset.setter
+    # def offset(self, value: int) -> None:
+    #     assert isinstance(value, int), f"{value} is not an int"
+    #     assert value >= 0, f"{value} is not a valid offset value"
+    #     self.offset_attr = value
+    #     self.get_reduce()
+    #
+    # @property
+    # def parents_in_name(self) -> int:
+    #     return self.parents_in_name_attr
+    #
+    # @parents_in_name.setter
+    # def parents_in_name(self, value: int) -> None:
+    #     assert isinstance(value, int), f"{value} is not an int"
+    #     assert value >= 0, f"{value} is not a valid parents_in_name value"
+    #     self.parents_in_name_attr = value
+    #     self.get_parents()
+
+
+class Video(ABC_Video):
+
+    def __init__(
+            self,
+            path: Path,
+            reduce: int = -1,
+            offset: int = 0,
+            parents_in_name: int = 0
+    ):
+        assert isinstance(path, Path_), f"{path} is not a Path object"
+        assert path.is_file(), f"{path} is not a file"
+        assert path.exists(), f"{path} does not exist"
+
+        assert isinstance(reduce, int), f"{reduce} is not an int"
+        assert reduce >= -1, f"{reduce} is not a valid reduce value"
+
+        assert isinstance(offset, int), f"{offset} is not an int"
+        assert offset >= 0, f"{offset} is not a valid offset value"
+
+        assert isinstance(parents_in_name, int), f"{parents_in_name} is not an int"
+        assert parents_in_name >= 0, f"{parents_in_name} is not a valid parents_in_name value"
+
+        # self._path: Path = path
+        #
+        # self._reduce: int = reduce
+        # self._offset: int = offset
+        # self._parents_in_name: int = parents_in_name
+
+        super().__init__(
+            path=path,
+            reduce=reduce,
+            offset=offset,
+            parents_in_name=parents_in_name
+        )
+
+        self.get_reduce()
+        self.get_parents()
+
+        self.get_info()
+
+
+class DummyVideo(ABC_Video):
+    def __init__(
+            self,
+            path: Path = Path("/home/pellipop/tv_show/season1/episode3.mp4"),
+            reduce: int = -1,
+            offset: int = 0,
+            parents_in_name: int = 0,
+    ):
+        path.resolve()
+        # super().__init__()
+        #
+        # self._path = path
+        # self._reduce = reduce
+        # self._offset = offset
+        # self._parents_in_name = parents_in_name
+        super().__init__(
+            path=path,
+            reduce=reduce,
+            offset=offset,
+            parents_in_name=parents_in_name
+        )
+
+    @property
+    def path(self) -> Path:
+        return self._path
+
+    @path.setter
+    def path(self, value: Path) -> None:
+        self._path = value
 
 
 if __name__ == "__main__":
